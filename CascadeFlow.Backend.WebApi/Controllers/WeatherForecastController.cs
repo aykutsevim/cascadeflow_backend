97856894cd4f4a8529c83034750ac1bb76f2d322
Aykut Sevim <aykutsevim@hotmail.com>
2024-05-07 08:53:18 +0300

    Trying

--- CascadeFlow.Backend.WebApi/Controllers/WeatherForecastController.cs
+++ CascadeFlow.Backend.WebApi/Controllers/WeatherForecastController.cs
@@ -9,8 +9,8 @@ namespace CascadeFlow.Backend.WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -24,6 +24,7 @@ namespace CascadeFlow.Backend.WebApi.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                Id = index,
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
