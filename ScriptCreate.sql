bd6cc974b4abdbc62fbed38cfeb4b764bfda665c
Aykut Sevim <aykutsevim@hotmail.com>
2024-05-28 00:32:57 +0300

    Added tenant, project definitions to db

--- ScriptCreate.sql
+++ ScriptCreate.sql
@@ -1,79 +1,47 @@
--- public.denemeentity definition
-
--- Drop table
-
--- DROP TABLE public.denemeentity;
-
-CREATE TABLE public.denemeentity (
-	id uuid NOT NULL,
-	kolon1s varchar NULL,
-	kolon2s varchar NULL,
-	kolon3n int8 NULL,
-	kolon4n int8 NULL,
-	CONSTRAINT denemeentity_pkey PRIMARY KEY (id)
+create table public.denemeentity
+(
+    id      uuid not null
+        primary key,
+    kolon1s varchar,
+    kolon2s varchar,
+    kolon3n bigint,
+    kolon4n bigint
 );
 
+alter table public.denemeentity
+    owner to postgres;
 
--- public.form definition
-
--- Drop table
-
--- DROP TABLE public.form;
-
-CREATE TABLE public.form (
-	id uuid NOT NULL,
-	formtitle varchar NULL,
-	entitytable varchar NULL,
-	CONSTRAINT form_pkey PRIMARY KEY (id)
+create table public.form
+(
+    id          uuid not null
+        primary key,
+    formtitle   varchar,
+    entitytable varchar
 );
 
+alter table public.form
+    owner to postgres;
 
--- public.formelement definition
-
--- Drop table
-
--- DROP TABLE public.formelement;
-
-CREATE TABLE public.formelement (
-	id uuid NOT NULL,
-	formref uuid NOT NULL,
-	dtypestring varchar NOT NULL,
-	"label" varchar NOT NULL,
-	fieldname varchar NULL,
-	listitems varchar NULL,
-	CONSTRAINT formelement_pkey PRIMARY KEY (id)
+create table public.formelement
+(
+    id          uuid    not null
+        primary key,
+    formref     uuid    not null,
+    dtypestring varchar not null,
+    label       varchar not null,
+    fieldname   varchar,
+    listitems   varchar
 );
 
-
--- public."user" definition
-
--- Drop table
-
--- DROP TABLE public."user";
-
-CREATE TABLE public."user" (
-	id uuid NOT NULL,
-	username varchar(60) NULL,
-	passwordhash bytea NULL,
-	passwordsalt bytea NULL,
-	refreshtoken varchar NULL,
-	refreshtokencreatedat timestamptz NULL,
-	refreshtokenexpiresat timestamptz NULL,
-	credate timestamptz NULL,
-	moddate timestamptz NULL,
-	"name" varchar NULL,
-	surname varchar NULL,
-	email varchar NULL,
-	phonenumber varchar NULL,
-	CONSTRAINT "User_pkey" PRIMARY KEY (id)
-);
+alter table public.formelement
+    owner to postgres;
 
 create table public.workitemtype
 (
-    id                  integer           not null
+    id               integer           not null
         constraint workitemtypepk
             primary key,
-    processref         integer default 1 not null,
+    processref       integer default 1 not null,
     workitemtypename varchar
 );
 
@@ -82,7 +50,7 @@ alter table public.workitemtype
 
 create table public.workitemstate
 (
-    id                   integer not null
+    id                integer not null
         constraint workitemstatepk
             primary key,
     workitemtyperef   integer not null
@@ -94,23 +62,77 @@ create table public.workitemstate
 alter table public.workitemstate
     owner to postgres;
 
+create table public.tenant
+(
+    id         uuid    not null
+        constraint tenant_pk
+            primary key,
+    tenantname varchar not null
+);
+
+alter table public.tenant
+    owner to postgres;
+
+create table public."user"
+(
+    id                    uuid not null
+        constraint "User_pkey"
+            primary key,
+    username              varchar(60),
+    passwordhash          bytea,
+    passwordsalt          bytea,
+    refreshtoken          varchar,
+    refreshtokencreatedat timestamp with time zone,
+    refreshtokenexpiresat timestamp with time zone,
+    credate               timestamp with time zone,
+    moddate               timestamp with time zone,
+    name                  varchar,
+    surname               varchar,
+    email                 varchar,
+    phonenumber           varchar,
+    tenantref             uuid not null
+        constraint user_tenant_id_fk
+            references public.tenant
+);
+
+alter table public."user"
+    owner to postgres;
+
+create table public.project
+(
+    id          uuid    not null
+        constraint project_pk
+            primary key,
+    projectname varchar not null,
+    tenantref   uuid    not null
+        constraint project_tenant_id_fk
+            references public.tenant
+);
+
+alter table public.project
+    owner to postgres;
 
 create table public.workitem
 (
-    id                  uuid    not null
+    id               uuid    not null
         constraint workitempk
             primary key,
     workitemtyperef  integer not null
         constraint workitemworkitemtypeidfk
             references public.workitemtype,
-    workitemstateref
-	 integer not null
+    workitemstateref integer not null
         constraint workitemworkitemstateidfk
             references public.workitemstate,
-    assignee            uuid,
-    title               varchar,
-    description         varchar,
-    priority            integer
+    assignee         uuid,
+    title            varchar,
+    description      varchar,
+    priority         integer,
+    projectref       uuid
+        constraint workitem_project_id_fk
+            references public.project,
+    workitemref      uuid
+        constraint workitem_workitem_id_fk
+            references public.workitem
 );
 
 alter table public.workitem
--- ScriptInsert.sql
+++ ScriptInsert.sql
@@ -1,79 +1,51 @@
-INSERT INTO public.denemeentity (id,kolon1s,kolon2s,kolon3n,kolon4n) VALUES
-	 ('2f4a55ce-2fa5-4de6-81ec-a5ad59c8b4e3','testkolon1','testkolon2',123,321);
-INSERT INTO public.form (id,formtitle,entitytable) VALUES
-	 ('41ba900d-7650-40bc-a79f-1fae2cc465be','Generic Form Backend','denemeentity');
-INSERT INTO public.formelement (id,formref,dtypestring,"label",fieldname,listitems) VALUES
-	 ('19958e5b-5166-4698-bbb0-77f04105baf3','41ba900d-7650-40bc-a79f-1fae2cc465be','GSelect','Country','kolon3n','[
-  { state: ''List item 1'', abbr: ''1'' },
-  { state: ''List item 2'', abbr: ''2'' },
-  { state: ''List item 3'', abbr: ''3'' },
-  { state: ''List item 4'', abbr: ''4'' },
-  { state: ''List item 5'', abbr: ''5'' },
-]'),
-	 ('72b59da6-2f3e-43b0-a955-37584434aa9a','41ba900d-7650-40bc-a79f-1fae2cc465be','GTextbox','Name','kolon1s',NULL),
-	 ('de857f82-5bd8-43f0-9526-3c5a5a0012ac','41ba900d-7650-40bc-a79f-1fae2cc465be','GTextbox','Surname','kolon2s',NULL),
-	 ('76559297-09af-408f-a820-dd1633137f03','41ba900d-7650-40bc-a79f-1fae2cc465be','GSelect','FieldType','kolon4n','[
-  { state: ''Other item 1'', abbr: ''1'' },
-  { state: ''Other item 2'', abbr: ''2'' },
-  { state: ''Other item 3'', abbr: ''3'' },
-  { state: ''Other item 4'', abbr: ''4'' },
-  { state: ''List item 5'', abbr: ''5'' },
+INSERT INTO public.form (id, formtitle, entitytable) VALUES ('41ba900d-7650-40bc-a79f-1fae2cc465be', 'Generic Form Backend', 'denemeentity');
+
+INSERT INTO public.formelement (id, formref, dtypestring, label, fieldname, listitems) VALUES ('19958e5b-5166-4698-bbb0-77f04105baf3', '41ba900d-7650-40bc-a79f-1fae2cc465be', 'GSelect', 'Country', 'kolon3n', e'[
+  { state: \'List item 1\', abbr: \'1\' },
+  { state: \'List item 2\', abbr: \'2\' },
+  { state: \'List item 3\', abbr: \'3\' },
+  { state: \'List item 4\', abbr: \'4\' },
+  { state: \'List item 5\', abbr: \'5\' },
 ]');
-INSERT INTO public."user" (id,username,passwordhash,passwordsalt,refreshtoken,refreshtokencreatedat,refreshtokenexpiresat,credate,moddate,"name",surname,email,phonenumber) VALUES
-	 ('fa7a12d9-1391-468c-b0f1-d56a0d328a7b','string',decode('DA310BF30EF57BD10F997A980990FB92B14A170BB7511A41E4FBE9719E2EE6529DB763A227822C59B5FC9EF3A589DC0B1ED4D5EF0B35732CBA81B4E1A431F4A8','hex'),decode('93934A5B3538AAD8CD7ECFE818D17A2FC121E4C55B10D406F5DCC5D8C7EBAA3581BAEE5FF81451943DC982740BCF00F2EC4F8F806442915A621FDC4A4EA24B35453AF7FF18264EB7C04A9C52674C2932868E8560106319911A538CFAD7C4FB9A0221508E28C5884F2E4B2CC7BF120B87875D15CF40B86E0843057D6C16FC1937','hex'),'QhSRm8pO9OS+MJbjiyDcbSXusU5vYi50UifG1brSP3n+Akfx7Q4JhXmPBZRmWtrZ0+dZdg79/d3iAJm13iUB3A==','2023-04-09 21:08:48.024294+03','2023-04-16 21:08:48.024293+03','2023-02-16 23:51:53.513567+03','2023-04-09 21:08:48.024593+03','Hasan','Kavak','hk@ella.com','03256565565'),
-	 ('094991dd-5106-4028-93b5-db37cccb25fb','username',decode('F5E0238B77DC5FDD076BC2B0156791494233DC5BE973C4C4CC1F403C8AB4C0AFD6B02ACDBD330A2157E95B877F58601D2AD79DA01F87D9A55B6B6FE5825DB2F3','hex'),decode('0A8C4A8802E895CCDAAA75BE46732165050319DF248F9349F653D2A9CFE19B15C3CA447D8914E18211E044ECC31B34105BC87C7622160BDB7C431E6E5CF45FCA769C988AE29288FAB0AE01F8B2E15A6EF860C12BA953F2290F9BE604D5E5930A34E4BBE94BDBFC39538FCCECC346AADAF74445427E276866F3EA3EB70AC19BD2','hex'),'FBe1uRAaVNajFeUwBcGiVDkL3p8IQy1+gsOT4/xnwcd2d+sYUPHrEm0QYVNNrDtgsBOS7yS4m1qMurz0/6OKDg==','2023-03-01 01:36:47.548919+03','2023-03-08 01:36:47.548919+03','2023-03-01 00:41:00.380964+03','2023-03-01 01:36:47.549215+03','Aykut','Sevim','as@ella.com','05322566565');
+INSERT INTO public.formelement (id, formref, dtypestring, label, fieldname, listitems) VALUES ('72b59da6-2f3e-43b0-a955-37584434aa9a', '41ba900d-7650-40bc-a79f-1fae2cc465be', 'GTextbox', 'Name', 'kolon1s', null);
+INSERT INTO public.formelement (id, formref, dtypestring, label, fieldname, listitems) VALUES ('de857f82-5bd8-43f0-9526-3c5a5a0012ac', '41ba900d-7650-40bc-a79f-1fae2cc465be', 'GTextbox', 'Surname', 'kolon2s', null);
+INSERT INTO public.formelement (id, formref, dtypestring, label, fieldname, listitems) VALUES ('76559297-09af-408f-a820-dd1633137f03', '41ba900d-7650-40bc-a79f-1fae2cc465be', 'GSelect', 'FieldType', 'kolon4n', e'[
+  { state: \'Other item 1\', abbr: \'1\' },
+  { state: \'Other item 2\', abbr: \'2\' },
+  { state: \'Other item 3\', abbr: \'3\' },
+  { state: \'Other item 4\', abbr: \'4\' },
+  { state: \'List item 5\', abbr: \'5\' },
+]');
 
+INSERT INTO public.denemeentity (id, kolon1s, kolon2s, kolon3n, kolon4n) VALUES ('2f4a55ce-2fa5-4de6-81ec-a5ad59c8b4e3', 'testkolon1', 'testkolon2', 123, 321);
 
-INSERT INTO public.workitemtype (id, processref, workitemtypename)
-VALUES (1::integer, DEFAULT, 'Task'::varchar);
+INSERT INTO public.tenant (id, tenantname) VALUES ('cd84c13b-b072-48d3-b4bd-65e746ff919c', 'kitlesel.com.tr');
+INSERT INTO public.tenant (id, tenantname) VALUES ('da61ee64-31dc-4302-8491-0f59112b6c28', 'oynio.com');
 
-INSERT INTO public.workitemtype (id, processref, workitemtypename)
-VALUES (2::integer, DEFAULT, 'Bug'::varchar);
+INSERT INTO public.project (id, projectname, tenantref) VALUES ('c212793b-d8b3-4aa8-8e42-42e1e34d77f4', 'oba-frontend', 'da61ee64-31dc-4302-8491-0f59112b6c28');
+INSERT INTO public.project (id, projectname, tenantref) VALUES ('666fcd2a-a67a-417c-aca8-63334fb9f0b1', 'oba-backend', 'da61ee64-31dc-4302-8491-0f59112b6c28');
 
-INSERT INTO public.workitemtype (id, processref, workitemtypename)
-VALUES (3::integer, DEFAULT, 'New Feature'::varchar);
+INSERT INTO public."user" (id, username, passwordhash, passwordsalt, refreshtoken, refreshtokencreatedat, refreshtokenexpiresat, credate, moddate, name, surname, email, phonenumber, tenantref) VALUES ('094991dd-5106-4028-93b5-db37cccb25fb', 'username', E'\\xF5E0238B77DC5FDD076BC2B0156791494233DC5BE973C4C4CC1F403C8AB4C0AFD6B02ACDBD330A2157E95B877F58601D2AD79DA01F87D9A55B6B6FE5825DB2F3', E'\\x0A8C4A8802E895CCDAAA75BE46732165050319DF248F9349F653D2A9CFE19B15C3CA447D8914E18211E044ECC31B34105BC87C7622160BDB7C431E6E5CF45FCA769C988AE29288FAB0AE01F8B2E15A6EF860C12BA953F2290F9BE604D5E5930A34E4BBE94BDBFC39538FCCECC346AADAF74445427E276866F3EA3EB70AC19BD2', 'FBe1uRAaVNajFeUwBcGiVDkL3p8IQy1+gsOT4/xnwcd2d+sYUPHrEm0QYVNNrDtgsBOS7yS4m1qMurz0/6OKDg==', '2023-02-28 22:36:47.548919 +00:00', '2023-03-07 22:36:47.548919 +00:00', '2023-02-28 21:41:00.380964 +00:00', '2023-02-28 22:36:47.549215 +00:00', 'Aykut', 'Sevim', 'as@ella.com', '05322566565', 'cd84c13b-b072-48d3-b4bd-65e746ff919c');
+INSERT INTO public."user" (id, username, passwordhash, passwordsalt, refreshtoken, refreshtokencreatedat, refreshtokenexpiresat, credate, moddate, name, surname, email, phonenumber, tenantref) VALUES ('fa7a12d9-1391-468c-b0f1-d56a0d328a7b', 'string', E'\\xDA310BF30EF57BD10F997A980990FB92B14A170BB7511A41E4FBE9719E2EE6529DB763A227822C59B5FC9EF3A589DC0B1ED4D5EF0B35732CBA81B4E1A431F4A8', E'\\x93934A5B3538AAD8CD7ECFE818D17A2FC121E4C55B10D406F5DCC5D8C7EBAA3581BAEE5FF81451943DC982740BCF00F2EC4F8F806442915A621FDC4A4EA24B35453AF7FF18264EB7C04A9C52674C2932868E8560106319911A538CFAD7C4FB9A0221508E28C5884F2E4B2CC7BF120B87875D15CF40B86E0843057D6C16FC1937', 'QhSRm8pO9OS+MJbjiyDcbSXusU5vYi50UifG1brSP3n+Akfx7Q4JhXmPBZRmWtrZ0+dZdg79/d3iAJm13iUB3A==', '2023-04-09 18:08:48.024294 +00:00', '2023-04-16 18:08:48.024293 +00:00', '2023-02-16 20:51:53.513567 +00:00', '2023-04-09 18:08:48.024593 +00:00', 'Hasan', 'Kavak', 'hk@ella.com', '03256565565', 'cd84c13b-b072-48d3-b4bd-65e746ff919c');
 
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (1::integer, 1::integer, 'To Do'::varchar);
+INSERT INTO public.workitemtype (id, processref, workitemtypename) VALUES (1, 1, 'Task');
+INSERT INTO public.workitemtype (id, processref, workitemtypename) VALUES (2, 1, 'Bug');
+INSERT INTO public.workitemtype (id, processref, workitemtypename) VALUES (3, 1, 'New Feature');
 
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (2::integer, 1::integer, 'In Progress'::varchar);
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (1, 1, 'To Do');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (2, 1, 'In Progress');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (3, 1, 'Done');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (4, 2, 'To Do');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (5, 2, 'In Progress');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (6, 2, 'Done');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (7, 3, 'Proposed');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (8, 3, 'To Do (Approved)');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (9, 3, 'In Progress');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (10, 3, 'Test');
+INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename) VALUES (11, 3, 'Done');
 
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (3::integer, 1::integer, 'Done'::varchar);
+INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority, projectref, workitemref) VALUES ('4262612d-d927-4a5a-8db1-5ffacb6ac608', 2, 1, null, 'Work Item 3', 'Detailed description', 1, '666fcd2a-a67a-417c-aca8-63334fb9f0b1', null);
+INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority, projectref, workitemref) VALUES ('1fabf2ad-4418-481c-b74a-74f5b428240b', 1, 2, null, 'Work Item 2', 'Detailed description', 2, '666fcd2a-a67a-417c-aca8-63334fb9f0b1', null);
+INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority, projectref, workitemref) VALUES ('19f09fe4-8b5e-4e06-bd51-94c6e889db53', 1, 1, null, 'Work Item 1', 'Detailed description', 1, '666fcd2a-a67a-417c-aca8-63334fb9f0b1', null);
+INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority, projectref, workitemref) VALUES ('6c86c51a-beb5-49e4-ae0a-69ea1f1f62c4', 1, 1, null, 'Child Item 2.1', 'Child', 2, '666fcd2a-a67a-417c-aca8-63334fb9f0b1', '1fabf2ad-4418-481c-b74a-74f5b428240b');
 
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (4::integer, 2::integer, 'To Do'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (5::integer, 2::integer, 'In Progress'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (6::integer, 2::integer, 'Done'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (7::integer, 3::integer, 'Proposed'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (8::integer, 3::integer, 'To Do (Approved)'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (9::integer, 3::integer, 'In Progress'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (10::integer, 3::integer, 'Test'::varchar);
-
-INSERT INTO public.workitemstate (id, workitemtyperef, workitemstatename)
-VALUES (11::integer, 3::integer, 'Done'::varchar);
-
-INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority)
-VALUES ('19f09fe4-8b5e-4e06-bd51-94c6e889db53'::uuid, 1::integer, 1::integer, null::uuid, 'Work Item 1'::varchar,
-        'Detailed description'::varchar, 1::integer);
-
-INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority)
-VALUES ('1fabf2ad-4418-481c-b74a-74f5b428240b'::uuid, 1::integer, 2::integer, null::uuid, 'Work Item 2'::varchar,
-        'Detailed description'::varchar, 2::integer);
-
-INSERT INTO public.workitem (id, workitemtyperef, workitemstateref, assignee, title, description, priority)
-VALUES ('4262612d-d927-4a5a-8db1-5ffacb6ac608'::uuid, 2::integer, 1::integer, null::uuid, 'Work Item 3'::varchar,
-        'Detailed description'::varchar, 1::integer);
